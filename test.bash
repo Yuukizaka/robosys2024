f7a7d0af5ea0bc010d2228d3f19a9f6a2557e937
Yuuki Ishizaka <yuukizk12jk@gmail.com>
2024-11-07 11:41:01 +0900

    ALL test

--- test.bash
+++ test.bash
@@ -1,13 +1,27 @@
-#!/bin/bash
-　
+#!/bin/bash -xv
+
 ng () {
-        echo ${1}行目が違うよ
-        res=1                   #追加
+	echo ${1}行目が違うよ
+	res=1
 }
-　
+
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数
-[ "$a" = 山田 ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
-　
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+　 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           
+[ "$?" = 1 ]      || ng "$LINENO" 
+[ "${out}" = "" ] || ng "$LINENO" 
+
+out=$(echo | ./plus)              
+[ "$?" = 1 ]      || ng "$LINENO" 
+[ "${out}" = "" ] || ng "$LINENO"
+
+
+[ "${res}" = 0 ] && echo OK
+
 exit $res
+
